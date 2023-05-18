6c9c1df935c886fede4714353e37b292c01e5d71
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-05-18 21:49:09 +0600

    Day 1

--- app/src/main/java/com/example/internetbrowser/MainActivity.java
+++ app/src/main/java/com/example/internetbrowser/MainActivity.java
@@ -2,13 +2,73 @@ package com.example.internetbrowser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText urlInput;
+    ImageView clearUrl;
+    WebView webView;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        urlInput=findViewById(R.id.url_input);
+        clearUrl=findViewById(R.id.clear_icon);
+        progressBar=findViewById(R.id.progress_bar);
+        webView = findViewById(R.id.web_view);
+
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                progressBar.setProgress(newProgress);
+            }
+        });
+    }
+
+    void loadMyUrl(String url){
+        boolean matchUrl= Patterns.WEB_URL.matcher(url).matches();
+        if(matchUrl){
+            webView.loadUrl(url);
+        }else {
+            webView.loadUrl("google.com/search?q="+url);
+        }
+    }
+    class MyWebViewClient extends WebViewClient{
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return false;
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            progressBar.setVisibility(View.INVISIBLE);
+        }
     }
 }
