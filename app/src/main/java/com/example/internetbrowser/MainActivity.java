6b1b66fc03c2f6e64d193b5fd1f6e7310718842f
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-05-20 22:56:07 +0600

    Day 2

--- app/src/main/java/com/example/internetbrowser/MainActivity.java
+++ app/src/main/java/com/example/internetbrowser/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.internetbrowser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 import android.util.Patterns;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
@@ -20,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     ImageView clearUrl;
     WebView webView;
     ProgressBar progressBar;
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setBuiltInZoomControls(true);
         webSettings.setDisplayZoomControls(false);
 
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new MyWebViewClient());
         webView.setWebChromeClient(new WebChromeClient(){
             @Override
             public void onProgressChanged(WebView view, int newProgress) {
@@ -43,6 +49,20 @@ public class MainActivity extends AppCompatActivity {
                 progressBar.setProgress(newProgress);
             }
         });
+
+        loadMyUrl("google.com");
+
+        urlInput.setOnEditorActionListener((v, i, event) -> {
+            if (i == EditorInfo.IME_ACTION_GO || i == EditorInfo.IME_ACTION_DONE){
+                InputMethodManager imm= (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(urlInput.getWindowToken(),0);
+                loadMyUrl(urlInput.getText().toString());
+                return true;
+            }
+            return false;
+        });
+        clearUrl.setOnClickListener(v -> urlInput.setText(""));
+
     }
 
     void loadMyUrl(String url){
@@ -53,6 +73,17 @@ public class MainActivity extends AppCompatActivity {
             webView.loadUrl("google.com/search?q="+url);
         }
     }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else {
+            super.onBackPressed();
+        }
+        super.onBackPressed();
+    }
+
     class MyWebViewClient extends WebViewClient{
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
